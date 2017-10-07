9cdd814155512310ec1811e584d128a916efa8fd
Jacek Gmoch <jacek.gmoch@gmail.com>
2017-10-07 11:07:27 +0200

    Zadanie 2 zrobione

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 namespace ConsoleLinqToObjects
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             string path = @"C:\Windows\";
-            GetFilesNamesAndSizeWithoutLinq(path);
+            //GetFilesNamesAndSizeWithoutLinq(path);
+            GetTop5LargestFileUsingLinq(path);
             Console.ReadLine();
         }
         private static void GetFilesNamesAndSizeWithoutLinq(string path)
@@ -18,11 +21,24 @@ namespace ConsoleLinqToObjects
             var directoryInfo = new DirectoryInfo(path);
             var filesInfo = directoryInfo.GetFiles();
             Array.Sort(filesInfo, new FileSizeComparer());
-            foreach (var fi in filesInfo)
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{filesInfo[i],-25} size: {filesInfo[i].Length,10:N0}");
+            }
+        }
+        private static void GetTop5LargestFileUsingLinq(string path)
+        {
+            var directoryInfo = new DirectoryInfo(path);
+            var filesInfo = from file in directoryInfo.GetFiles()
+                            orderby file.Length descending
+                            select file;
+            foreach (var fi in filesInfo.Take(5))
             {
                 Console.WriteLine($"{fi.Name,-25} size: {fi.Length,10:N0}");
             }
         }
+    }
+
     }
     public class FileSizeComparer : IComparer<FileInfo>
     {
@@ -31,4 +47,3 @@ namespace ConsoleLinqToObjects
             return y.Length.CompareTo(x.Length);
         }
     }
-}
